d3946bcb0f60469c38779e9e962d25217153aa45
Vinai Kopp <vinai@netzarbeiter.com>
2012-05-17 00:22:20 +0200

    Added basic results processing

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var routes  = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -28,8 +28,10 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+
+// /run?target=local6.dev
+app.get('/run', routes.run);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,45 @@
 
+var mage = require('mage-finger');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { pageTitle: 'Mage::finger()' })
+};
+
+
+/*
+ * Process target
+ */
+
+exports.run = function(req, res, next){
+    var target = req.param('target', false);
+    if (! target) {
+      next(new Error('No target specified'));
+    }
+    if (! target.match(/^https?:\/\//)) {
+        target = 'http://' + target;
+    }
+    if (! target.substr(-1) == '/') {
+        target += '/';
+    }
+
+    var counter = 0;
+    var collector = new mage.collector(target, {
+        'scoreLimit': 2,
+        //'resultLimit': 6,
+        'testcomplete': function(finger, versions) {
+            console.log('Completed test "' + finger + '"');
+            counter++;
+        },
+        'allcomplete': function(results) {
+            console.log('Finished ' + counter + ' test(s)');
+            console.log("\n");
+
+            res.render('run', { pageTitle: 'Processing ' + target, target: target, results: results });
+        }
+    });
+    collector.finger();
 };
